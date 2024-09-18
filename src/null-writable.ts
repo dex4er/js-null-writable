9e79f85a66d37091c15e05ef4e8c7327b9d50ba0
Björn Kautler <Bjoern@Kautler.net>
2024-09-18 04:10:18 +0200

    Adjust signatures to upstream

--- src/null-writable.ts
+++ src/null-writable.ts
@@ -3,10 +3,10 @@
 import {Writable} from "node:stream"
 
 export class NullWritable extends Writable {
-  _write(_chunk: any, _encoding: string, callback: (error?: Error | null) => void): void {
+  _write(_chunk: any, _encoding: BufferEncoding, callback: (error?: Error | null) => void): void {
     callback()
   }
-  _writev(_chunks: Array<{chunk: any; encoding: string}>, callback: (error?: Error | null) => void): void {
+  _writev?(_chunks: Array<{chunk: any; encoding: BufferEncoding}>, callback: (error?: Error | null) => void): void {
     callback()
   }
 }
